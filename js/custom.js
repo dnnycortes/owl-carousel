eb558e6d80b10ec7c333e88eb6c0a078451dbc49
Daniela Munoz <daniela.munoz@enova.mx>
2018-04-03 18:47:50 -0500

    First owl carousel commit

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+       nav: true,
+       items: 1,
+       mouseDrag: true,
+       touchDrag: true, // for responsive view
+       dots: true
+    });
+});
